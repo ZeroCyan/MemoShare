9fddded427479eeb966173142b4d2026d4e5c46d
tvdb <user@email.com>
2024-02-24 17:15:37 +0100

    Refactoring: the expiration date is calculated at object creation

--- src/main/java/be/pbin/writeserver/data/metadata/NoteMetadataRepository.java
+++
@@ -1,6 +0,0 @@
-package be.pbin.writeserver.data.metadata;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface NoteMetadataRepository extends CrudRepository<NoteMetaData, String> {
-}
